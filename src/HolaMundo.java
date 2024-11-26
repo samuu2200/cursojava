da928b292c0321da65572fc839fbb9d8dd59825f
Tardes <Tardes@GET-05>
2024-11-26 17:46:30 +0100

    modificación para añadir nuevo valor

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -2,7 +2,7 @@
 public class HolaMundo {
 	public static void main (String[] args) {
 		//Comentario de línea
-		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo2");
 		
 		/*
 		 * Comentario 1
