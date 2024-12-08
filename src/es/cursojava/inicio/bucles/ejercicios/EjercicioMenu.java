3a0f10be16c125a590a26e689062ac0af1b7f7e1
daroscode <daroscode@192.168.0.100>
2024-12-08 10:05:13 +0100

    Ejercicio del menú, realizado en casa

--- src/es/cursojava/inicio/bucles/ejercicios/EjercicioMenu.java
+++ src/es/cursojava/inicio/bucles/ejercicios/EjercicioMenu.java
@@ -4,52 +4,108 @@ import java.util.Scanner;
 
 public class EjercicioMenu {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		
+
 		Scanner scan = new Scanner(System.in);
-		
-		System.out.println("Introduce la opción en número que quieres elegir: (1.Pintar Cuadro, 2.Validar email, 3.Añadir Alumno, 4.Salir): ");
-		int opcion = scan.nextInt();
-		final int OPCION_SALIR = 4;
+		int option = 0;
 		
 		do {
 			
-			switch (opcion) {
-				case 1 :
-					
-					System.out.println("Introduce el tamaño del cuadrado ");
-					int size = scan.nextInt();
-					
-					for (int row = 0; row < size; row++) {
-						
-						for (int col = 0; col < size; col++) {
-							
-							if (row == 0 || row == size -1 || col == 0 || col == size -1) {
-								System.out.print(" * ");
-							}else {
-								System.out.print("   ");
+			System.out.println("**** MENÚ ****");
+			System.out.println("1. Pintar Cuadro");
+			System.out.println("2. Validar Email");
+			System.out.println("3. Añadir Alumno");
+			System.out.println("4. Salir");
+			System.out.println("Elige una opción:");
+			option = scan.nextInt();
+			scan.nextLine();
+			
+			if (option == 1) {
+				//Pintar Cuadro
+				System.out.println("Introduce el tamaño del cuadrado\n");
+				int size = scan.nextInt();
+				
+				if (size < 1) {
+					System.out.println("El tamaño del cuadrado debe ser mayor a 1");
+				} else {
+					for (int width = 1; width <= size; width++) {
+						for (int height = 1; height <= size; height++) {
+							if (width == 1 || width == size || height == 1 || height == size) {
+								System.out.print("* ");
+							} else {
+								System.out.print("  ");
 							}
-							
 						}
 						System.out.println();
-						
 					}
-					
+				}
+			} else if (option == 2) {
+				//Validar Email
+				System.out.println("Introduce el email a validar:\n");
+				String email = scan.nextLine();
 				
-				case 2 :
-					
+				//Acumulamos los mensajes de error
+				StringBuilder errores = new StringBuilder();
 				
-				case 3 : System.out.println();
-				case 4 : System.out.println();
-				default : System.out.println();
+				//Verificar que tenga solo una @
+				int countArrobas = 0;
+				for (int i = 0; i < email.length(); i++) {
+					if (email.charAt(i) == '@') countArrobas++; 
+				}
+				if (countArrobas != 1) {
+					errores.append("- El email debe contener exactamente un '@'\n");
+				}
+				
+				//Verificar que tenga espacios en blanco
+				if (email.contains(" ")) {
+					errores.append("- El email no puede contener espacios en blanco\n");
+				}
+				
+				//Verificar posiciones
+				int posicionArroba = email.indexOf('@');
+				int posicionUltPunto = email.lastIndexOf('.');
+				int posicionPuntoAntesArroba = email.lastIndexOf('.', posicionArroba - 1);
+				
+				if (posicionUltPunto < posicionArroba || posicionUltPunto == -1) {
+					errores.append("- El email debe contener un punto después de la '@'\n");
+				}
+				if (posicionPuntoAntesArroba != -1 && posicionUltPunto > posicionArroba) {
+					errores.append("- El email no puede tener un punto antes la '@' y después\n");
+				}
+				//Verificar que después de la @ hayan al menos 2 caracteres
+				if (posicionUltPunto > posicionArroba + 1 && posicionUltPunto - posicionArroba <= 2) {
+					errores.append("- El email debe tener al menos dos caracteres después de la '@'\n");
+				}
+				
+				//Verificar que después del punto solo hayan más de 2 y menos que 6 caracateres
+				int caracteresDespuesPunto = email.length()	- posicionUltPunto - 1;
+				if (caracteresDespuesPunto < 2 || caracteresDespuesPunto > 6) {
+					errores.append("- El email debe tener más de 2 y menos que 6 caracteres después del punto.\n");
+				}
+				
+				//Mostrar por consola
+				if (errores.length() == 0) {
+					System.out.println("El email es válido");
+				} else {
+					System.out.println("El email es inválido");
+					System.out.println(errores.toString());
+				}
+				
+				
+			} else if (option == 3) {
+				//Añadir Alumno
+				
+			} else if (option == 4) {
+				//Salir del menú
+				
+			} else {
 				
 			}
 			
-		}while (opcion != OPCION_SALIR);
+		} while (option != 4);
+		
+		scan.close();
 		
 	}
-	
+
 }
