b025629208ddfbee23ad836734c5297ad6843a9e
Tardes <Tardes@GET-05>
2024-12-12 17:38:43 +0100

    Ejercicio bucle  3

---
+++ src/es/cursojava/inicio/arrays/ejercicio/Ejercicio3.java
@@ -0,0 +1,21 @@
+package es.cursojava.inicio.arrays.ejercicio;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		
+		//Meter los valores en un array
+		int[] numeros = {4,8,15,16,23,42};
+
+		int suma = 0;
+		for (int numero : numeros) {
+			suma = numero + suma;
+		}
+		
+		int media = suma / numeros.length;
+		
+		System.out.println(media);
+		
+	}
+	
+}
