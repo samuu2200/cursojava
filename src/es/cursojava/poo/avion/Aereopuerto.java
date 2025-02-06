98b89bd4077f34e1cd12720be06ed7a7f89c4a55
samdev2200 <samuel2200@hotmail.com>
2025-02-06 17:22:10 +0100

    cambios eclipse

--- src/es/cursojava/poo/avion/Aereopuerto.java
+++ src/es/cursojava/poo/avion/Aereopuerto.java
@@ -3,8 +3,8 @@ package es.cursojava.poo.avion;
 public class Aereopuerto {
 
     public static void main(String[] args) {
-        Avion avion = new Avion(0, null, 0);
-        avion.getCapacidad();
+        //Avion avion = new Avion(0, null, 0);
+        //avion.getCapacidad();
     }
     
 }
